46b8a7b16345b5bdf1b9d5f986ceef426aa8450e
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-09-28 14:43:20 +0530

    Add utils

---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+const minSizedBoxWidth = 2.0;
+
+const listDataPadding = EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0);
+
+const listDataBoxDecoration = BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+    color: Color(0x4Dc1c8c7));
+
+const listHeaderDataBoxDecoration = BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+    color: Color(0xCCFFFFFF));
