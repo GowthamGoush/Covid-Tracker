c1601854e4e632fe430bc317fbaa59cdacb34970
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-09-28 14:44:47 +0530

    Add screens

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,89 @@
+import 'package:covid_tracker_app/models/covid_data_model.dart';
+import 'package:covid_tracker_app/screens/tracker_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+//Http package
+import 'package:http/http.dart' as http;
+
+// JSON converter
+import 'dart:convert';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  LoadingScreenState createState() => new LoadingScreenState();
+}
+
+class LoadingScreenState extends State<LoadingScreen> {
+  String _url = 'https://api.covid19india.org/state_district_wise.json';
+  Map<String, dynamic> covidData = {};
+  List<CovidDataModel> cityData = [];
+  List<CovidDataModel> stateData = [];
+
+  void _getCovidData() async {
+    final response = await http.get(
+      Uri.parse(_url),
+      headers: {"Accept": "application/json"},
+    );
+
+    if (response.statusCode != 200) {
+      return;
+    }
+
+    covidData = jsonDecode(response.body);
+
+    covidData.forEach((key, value) {
+      int totalActive = 0;
+      int totalRecovered = 0;
+      int totalConfirmed = 0;
+      int totalDeceased = 0;
+
+      value['districtData'].forEach((key, value) {
+        cityData.add(new CovidDataModel(key, value['active'],
+            value['recovered'], value['confirmed'], value['deceased']));
+
+        totalActive += int.parse(value['active'].toString()) > 0
+            ? int.parse(value['active'].toString())
+            : 0;
+        totalRecovered += int.parse(value['recovered'].toString()) > 0
+            ? int.parse(value['recovered'].toString())
+            : 0;
+        totalConfirmed += int.parse(value['confirmed'].toString()) > 0
+            ? int.parse(value['confirmed'].toString())
+            : 0;
+        totalDeceased += int.parse(value['deceased'].toString()) > 0
+            ? int.parse(value['deceased'].toString())
+            : 0;
+      });
+
+      stateData.add(new CovidDataModel(
+          key, totalActive, totalRecovered, totalConfirmed, totalDeceased));
+    });
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return TrackerScreen(
+        covidData: this.cityData,
+        stateData: this.stateData,
+      );
+    }));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getCovidData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SpinKitChasingDots(
+          color: Colors.red,
+          size: 50,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/tracker_screen.dart
@@ -0,0 +1,143 @@
+import 'package:covid_tracker_app/models/covid_data_model.dart';
+import 'package:covid_tracker_app/widgets/delegate_persistent_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:covid_tracker_app/widgets/list_region_data.dart';
+
+class TrackerScreen extends StatefulWidget {
+  TrackerScreen({required this.covidData, required this.stateData});
+
+  final List<CovidDataModel> covidData;
+  final List<CovidDataModel> stateData;
+
+  @override
+  TrackerScreenState createState() => new TrackerScreenState();
+}
+
+class TrackerScreenState extends State<TrackerScreen> {
+  bool isState = false;
+  var currentSelectedValue;
+
+  void changeToState() {
+    setState(() {
+      isState = !isState;
+    });
+  }
+
+  String convertToString(int value) {
+    String convertedString;
+
+    value > 1000000
+        ? convertedString = (value / 1000000).round().toString() + 'M'
+        : value > 1000
+            ? convertedString = (value / 1000).round().toString() + 'K'
+            : convertedString = value.toString();
+
+    return convertedString;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          slivers: [
+            SliverAppBar(
+              expandedHeight: 250,
+              floating: false,
+              pinned: false,
+              backgroundColor: Colors.white,
+              actions: [
+                DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    items: <String>['City', 'State/UT'].map((String value) {
+                      currentSelectedValue = value;
+
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (newValue) {
+                      currentSelectedValue = newValue;
+
+                      if ((newValue == 'State/UT' && !isState) ||
+                          (newValue == 'City' && isState)) changeToState();
+                    },
+                  ),
+                ),
+              ],
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const Text(
+                      'Covid-19 Tracker',
+                      style: TextStyle(color: Colors.white, fontSize: 16.0),
+                      textAlign: TextAlign.end,
+                    ),
+                  ],
+                ),
+                background: Image.asset(
+                  'assets/images/tracker_screen_background.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SliverPersistentHeader(
+              delegate: Delegate(isState: isState),
+              pinned: true,
+              floating: true,
+            ),
+            SliverList(
+                delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                if (index == 0) {
+                  return Container();
+                }
+
+                String regionName;
+                int confirmed;
+                int active;
+                int recovered;
+                int deceased;
+
+                if (isState) {
+                  regionName = widget.stateData[index].districtName;
+                  confirmed = widget.stateData[index].confirmed;
+                  active = widget.stateData[index].active;
+                  recovered = widget.stateData[index].recovered;
+                  deceased = widget.stateData[index].deceased;
+                } else {
+                  regionName = widget.covidData[index].districtName;
+                  confirmed = widget.covidData[index].confirmed;
+                  active = widget.covidData[index].active;
+                  recovered = widget.covidData[index].recovered;
+                  deceased = widget.covidData[index].deceased;
+                }
+
+                return RegionData(
+                  districtName: regionName,
+                  confirmed: confirmed == 0 ? '-' : convertToString(confirmed),
+                  active: active == 0 ? '-' : convertToString(active),
+                  recovered: recovered == 0 ? '-' : convertToString(recovered),
+                  deceased: deceased == 0 ? '-' : convertToString(deceased),
+                );
+              },
+              childCount:
+                  isState ? widget.stateData.length : widget.covidData.length,
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
