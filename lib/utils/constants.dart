a95472d78ec82b97076580a3e043e1b423c7a728
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-09-29 11:41:17 +0530

    Update constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -6,8 +6,19 @@ const listDataPadding = EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0);
 
 const listDataBoxDecoration = BoxDecoration(
     borderRadius: BorderRadius.all(Radius.circular(4.0)),
-    color: Color(0x4Dc1c8c7));
+    color: Color(0x80e8ecec));
 
 const listHeaderDataBoxDecoration = BoxDecoration(
     borderRadius: BorderRadius.all(Radius.circular(4.0)),
     color: Color(0xCCFFFFFF));
+
+const appBarTitle = Text(
+  'Covid-19 Tracker',
+  style: TextStyle(
+    color: Color(0x0000ff),
+    fontFamily: 'Spartan',
+    fontWeight: FontWeight.bold,
+    fontSize: 16.0,
+  ),
+  textAlign: TextAlign.end,
+);
